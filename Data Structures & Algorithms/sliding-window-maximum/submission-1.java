920bc4665c81ccd6e3a9879e05dce23b2238cb50
Raushan Singh <19raushaniiitu@gmail.com>
2026-04-18 03:29:26 +0530

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] arr, int k) {
+        if(arr == null || arr.length == 0)return new int[]{};
+        int n = arr.length;
+        int[]result = new int[n-k+1];
+        Deque<Integer>dq = new LinkedList();
+        for(int i = 0; i < n;i++){
+            // remove out of window
+            if(!dq.isEmpty() && dq.peekFirst() == i-k){
+                dq.pollFirst();
+            }
+            // maintain decreasing order
+            while(!dq.isEmpty() && arr[dq.peekLast()] < arr[i]){
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+
+            // Store result
+            if(i >= k-1){
+                result[i+1-k] = arr[dq.peekFirst()];
+            }
+        }
+        
+        return result;
+        // Deque<Integer> dq = new LinkedList<>();
+        // int n = nums.length;
+        // int[] res = new int[n - k + 1];
+
+        // for (int i = 0; i < n; i++) {
+        //     // Remove out of window
+        //     if (!dq.isEmpty() && dq.peekFirst() == i - k) {
+        //         dq.pollFirst();
+        //     }
+
+        //     // Maintain decreasing order
+        //     while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
+        //         dq.pollLast();
+        //     }
+
+        //     dq.offerLast(i);
+
+        //     // Store result
+        //     if (i >= k - 1) {
+        //         res[i - k + 1] = nums[dq.peekFirst()];
+        //     }
+        // }
+
+        // return res;
+    }
+}
+
+
