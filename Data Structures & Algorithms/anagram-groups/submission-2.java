6112cf239c57620894fbd8bc13ac3c1683deff96
Raushan Singh <19raushaniiitu@gmail.com>
2026-04-17 14:01:17 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs == null || strs.length == 0)return new ArrayList();
+        int m = strs.length;
+        Map<String, List<String>>anagrams = new HashMap();
+        for(String str: strs){
+            int[]frq = new int[26];
+            for(char ch: str.toCharArray()){
+                frq[ch-'a']++;
+            }
+            StringBuilder keyBuilder = new StringBuilder();
+            for(int cnt : frq)keyBuilder.append(cnt).append("#");
+            String key = keyBuilder.toString();
+            anagrams.computeIfAbsent(key, r->new ArrayList()).add(str);
+        }
+        return new ArrayList(anagrams.values());
+        // if(strs == null || strs.length == 0)return new ArrayList();
+        // int m = strs.length;
+        // Map<String,List<String>>anagramStringMap = new HashMap();
+        // for(String str: strs){
+        //     int[]frq = new int[26];
+        //     for(char ch : str.toCharArray()){
+        //         frq[ch-'a']++;
+        //     }
+        //     StringBuilder keyBuilder = new StringBuilder();
+        //     for(int cnt: frq)keyBuilder.append(cnt).append("#");
+        //     String key = keyBuilder.toString();
+        //     // Grouping logic
+        //     if(!anagramStringMap.containsKey(key)){
+        //         anagramStringMap.put(key, new ArrayList());
+        //     }
+        //     anagramStringMap.get(key).add(str);
+        // }
+        // List<List<String>>result = new ArrayList();
+        // for(Map.Entry<String,List<String>>e:anagramStringMap.entrySet()){
+        //     result.add(e.getValue());
+        // }
+        // return result;
+    }
+}
