8e5e92693b3de7382e4dee3e354ee8fca8b1923c
Raushan Singh <19raushaniiitu@gmail.com>
2026-04-21 20:14:03 +0530

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean searchMatrix(int[][] grid, int target) {
+        if(grid == null || grid.length == 0)return false;
+        int m = grid.length, n = grid[0].length;
+        int r = 0, c = n-1;
+        // tc - O ( m + n) m -> number of rows and n  -> number of cols 
+        // sc - O(1)
+        while(r < m && c >= 0){
+            if(grid[r][c] > target){
+                c--;
+            }else if ( grid[r][c] < target){
+                r++;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
