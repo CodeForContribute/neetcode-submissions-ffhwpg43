e05976fead29fcb00ef18198376f7120be98e8f9
Raushan Singh <19raushaniiitu@gmail.com>
2026-04-30 21:07:59 +0530

    Add: count-number-of-islands - submission-2

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-2.java
@@ -0,0 +1,34 @@
+class Solution {
+    private static final int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    private void bfs(char[][]grid, int r, int c, int m, int n){
+        Queue<int[]>q = new LinkedList();
+        q.offer(new int[]{r,c});
+        while(!q.isEmpty()){
+            int[]node = q.poll();
+            int row = node[0],col = node[1];
+            grid[row][col] = '0';
+            for(int[]dir: directions){
+                int nr = row + dir[0],nc = col + dir[1];
+                if(nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == '1'){
+                   //grid[nr][nc] = '0';
+                    q.offer(new int[]{nr,nc});
+                }
+            }
+        }
+
+    }
+    public int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0)return 0;
+        int m = grid.length, n = grid[0].length;
+        int ans = 0;
+        for(int r = 0; r < m;r++){
+            for(int c = 0; c < n;c++){
+                if(grid[r][c] == '1'){
+                    ans++;
+                    bfs(grid,r,c,m,n);
+                }
+            }
+        }
+        return ans;
+    }
+}
