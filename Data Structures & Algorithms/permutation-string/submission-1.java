06cd14839b91f8d9dade778aedb2f82a262c120b
Raushan Singh <19raushaniiitu@gmail.com>
2026-05-23 19:36:36 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean checkInclusion(String a, String b) {
+        if(a == null &&  b == null)return true;
+        if(a == null || b == null)return false;
+        if(a.length() > b.length())return false;
+        int m = a.length(), n = b.length();
+        int[]aFrq = new int[26], bFrq = new int[26];
+        int left = 0, right = 0;
+        for(char ch : a.toCharArray())aFrq[ch-'a']++;
+        while(right < n){
+            char ch = b.charAt(right);
+            bFrq[ch-'a']++;
+            while(right - left + 1 > m){
+                char c = b.charAt(left);
+                bFrq[c-'a']--;
+                left++;
+            }
+            if(right-left+1 == m && Arrays.equals(aFrq,bFrq))return true;
+            right++;
+        }
+        return false;
+        
+        // int[]permuteFrq = new int[26];
+        // while(right < n){
+        //     char ch = s2.charAt(right);
+        //     permuteFrq[ch-'a']++;
+        //     // variable window - while be usefult
+        //     // fixed window - if will be useful as it is moved only once.
+        //     while(right - left + 1 > k){
+        //         char c = s2.charAt(left);
+        //         permuteFrq[c-'a']--;
+        //         left++;
+        //     }
+        //     if(right - left + 1 == k && Arrays.equals(permuteFrq,s1CharFrq))return true;
+        //     right++;
+        // }
+        // return false;
+    }
+}
