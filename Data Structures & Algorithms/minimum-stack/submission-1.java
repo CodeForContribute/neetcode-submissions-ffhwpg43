365fdf6e04451f9a4168305ad2347e6dc14e3073
Raushan Singh <19raushaniiitu@gmail.com>
2026-05-24 03:43:12 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,65 @@
+class MinStack {
+    private Stack<Integer>stack;
+    private Stack<Integer>minStack;
+
+    public MinStack() {
+        this.stack = new Stack();
+        this.minStack = new Stack();    
+    }
+    
+    public void push(int val) {
+        this.stack.push(val);
+        if(minStack.isEmpty() || val <= minStack.peek())
+            minStack.push(val);
+    }
+    
+    public void pop() {
+        int val = this.stack.pop();
+        if(val <= minStack.peek())minStack.pop();
+        
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/*
+class MinStack {
+
+    private Stack<Integer>stack;
+    private Stack<Integer>minStack;
+
+    public MinStack() {
+        this.stack = new Stack();
+        this.minStack = new Stack();    
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(minStack.isEmpty() || minStack.peek() >= val){
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        if(stack.isEmpty())return;
+        int top = stack.pop();
+        if(top == minStack.peek()){
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+*/
