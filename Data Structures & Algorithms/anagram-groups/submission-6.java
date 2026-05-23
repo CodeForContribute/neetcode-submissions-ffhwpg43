c9ce42fa9b0bfaab64733ebbe963d488f2281da5
Raushan Singh <19raushaniiitu@gmail.com>
2026-05-23 23:15:14 +0530

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs == null || strs.length == 0)return new ArrayList();
+        int m = strs.length;
+        Map<String,List<String>>anagrams = new HashMap();
+        for(String str:strs){
+            int[]frq = new int[26];
+            for(char ch : str.toCharArray())frq[ch-'a']++;
+            StringBuilder sb = new StringBuilder();
+            for(int cnt : frq)sb.append(cnt).append('#');
+            String key = sb.toString();
+            anagrams.computeIfAbsent(key, r->new ArrayList()).add(str);
+        }
+        return new ArrayList(anagrams.values());
+        // if(strs == null || strs.length == 0)return new ArrayList();
+        // int m = strs.length;
+        // Map<String, List<String>>anagrams = new HashMap();
+        // for(String str: strs){
+        //     int[]frq = new int[26];
+        //     for(char ch: str.toCharArray()) frq[ch-'a']++;
+        //     StringBuilder keyBuilder = new StringBuilder();
+        //     for(int cnt : frq)keyBuilder.append(cnt).append("#");
+        //     String key = keyBuilder.toString();
+        //     anagrams.computeIfAbsent(key, r->new ArrayList()).add(str);
+        // }
+        // return new ArrayList(anagrams.values());
+    }
+}
