31f692121b78c8edc42a50b05df5bb62e4343f9f
Raushan Singh <19raushaniiitu@gmail.com>
2026-05-24 01:56:06 +0530

    Add: valid-binary-search-tree - submission-7

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-7.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if(root == null)return true;
+        // return dfs(root,Long.MIN_VALUE, LONG.MAX_VALUE);
+        return bfs(root,Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean bfs(TreeNode root, long lower, long upper){
+        if(root == null)return true;
+        Queue<Object[]>q = new LinkedList();
+        q.offer(new Object[]{root, lower, upper});
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i < size;i++){
+                Object[] obj = q.poll();
+                TreeNode node = (TreeNode)obj[0];
+                lower = (long) obj[1];
+                upper = (long) obj[2];
+                if(node.val <= lower || node.val >= upper)return false;
+                if(node.left != null)q.offer(new Object[]{node.left, lower, (long)node.val});
+                if(node.right != null)q.offer(new Object[]{node.right, (long)node.val, upper});
+            }
+        }
+        return true;
+    }
+    private boolean dfs(TreeNode root, long lower, long upper){
+        if(root == null)return true;
+        if(root.val <= lower || root.val >= upper)return false;
+        return dfs(root.left, lower, root.val) && dfs(root.right, root.val, upper);
+    }
+}
