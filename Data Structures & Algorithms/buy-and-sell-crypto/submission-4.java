bf29ffdf93e36a16e40462a9ec0bbd53eaa002b3
Raushan Singh <19raushaniiitu@gmail.com>
2026-05-23 17:58:37 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length == 0)return 0;
+        int n = prices.length;
+        int left = 0, right = 1;
+        int maxProfit = 0;
+        while(right < n){
+            if(prices[left] < prices[right]){
+                int profit = prices[right] - prices[left];
+                maxProfit = Math.max(maxProfit, profit);
+            }else{
+                left = right;
+            }
+            right++;
+        }
+        return maxProfit;
+        // if(prices == null || prices.length == 0)return 0;
+        // int n = prices.length;
+        // int left = 0, right = 1;
+        // int maxProfit = 0;
+        // while(right < n){
+        //     if(prices[left] < prices[right]){
+        //         int profit = prices[right] - prices[left];
+        //         maxProfit = Math.max(maxProfit, profit);
+        //     }else{
+        //         left = right;
+        //     }
+        //     right++;
+        // }
+        // return maxProfit;
+    }
+}
