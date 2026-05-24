230856c6283d6247e279fd8580f0140627613986
Raushan Singh <19raushaniiitu@gmail.com>
2026-05-25 01:30:15 +0530

    Add: number-of-one-bits - submission-2

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int hammingWeight(int n) {
+        // bitmast optional
+        int result = 0;
+        while( n != 0){
+            n &= n-1;
+            result++;
+        }
+        return result;
+        // int result = 0;
+        // while(n != 0){
+        //     result += ( n & 1) == 1 ? 1 : 0;
+        //     n >>= 1;
+        // }
+        // return result;
+        // int ans = 0;
+        // for(int i = 0; i < 32; i++){
+        //     int bitmask = (1 << i);
+        //     if((bitmask & n) != 0){
+        //         ans++;
+        //     }
+        // }
+        // return ans;
+    }
+}
