dc9d72f1d533f6a20175a03bfb68fd698175386a
Raushan Singh <19raushaniiitu@gmail.com>
2026-06-07 14:54:10 +0530

    Add: rotate-matrix - submission-1

---
+++ Data Structures & Algorithms/rotate-matrix/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void rotate(int[][] grid) {
+        // swap the rows and cols in grid
+        // then reverse the cols
+        if(grid == null || grid.length == 0)return;
+        int m = grid.length, n = grid[0].length;
+        for(int r = 0; r < m;r++){
+            for(int c = r ; c < n;c++){
+                swap(grid,r,c);
+            }
+        }
+        for(int r = 0; r < n;r++){
+            for(int c = 0; c < n/2; c++){
+                int temp = grid[r][c];
+                grid[r][c] = grid[r][n-c-1];
+                grid[r][n-c-1] = temp;
+            }
+        }
+    }
+
+    private void swap(int[][] grid, int r, int c){
+        int temp = grid[r][c];
+        grid[r][c] = grid[c][r];
+        grid[c][r] = temp;
+    }
+}
