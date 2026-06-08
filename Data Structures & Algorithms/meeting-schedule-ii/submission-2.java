63d5a2237e86ecf15e502740edd8a60f7270c198
Raushan Singh <19raushaniiitu@gmail.com>
2026-06-08 16:00:34 +0530

    Add: meeting-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-2.java
@@ -0,0 +1,37 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        if(intervals == null || intervals.size() == 0)return 0;
+        int n = intervals.size();
+        int[]start = new int[n], end = new int[n];
+        for(int i = 0; i < n;i++){
+            start[i] = intervals.get(i).start;
+            end[i] = intervals.get(i).end;
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+
+        int result = 0, cnt = 0, s = 0, e = 0;
+        while(s < n){
+            if(start[s] < end[e]){
+                s++;
+                cnt++;
+            }else{
+                e++;
+                cnt--;
+            }
+            result = Math.max(result, cnt);
+        }
+        return result;
+    }
+}
