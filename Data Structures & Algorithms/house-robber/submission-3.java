54f51506975ad156f7e54cf9288f252624acab0b
Raushan Singh <19raushaniiitu@gmail.com>
2026-06-10 23:37:04 +0530

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    private int[]dp;
+    public int rob(int[] arr) {
+        if(arr == null || arr.length == 0)return 0;
+        int n = arr.length;
+        dp = new int[n];
+        Arrays.fill(dp,-1);
+        return dfs(arr,0);
+    }
+    private int dfs(int[]arr, int idx){
+        if(idx >= arr.length)return 0;
+        if(dp[idx] != -1)return dp[idx];
+        dp[idx] = Math.max(dfs(arr,idx+1), arr[idx] + dfs(arr, idx+2));
+        return dp[idx];
+    }
+}
