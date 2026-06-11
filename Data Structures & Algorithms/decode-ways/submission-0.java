b160d83f2374ddc272c40c3d14c223b1381aab1c
Raushan Singh <19raushaniiitu@gmail.com>
2026-06-11 13:30:54 +0530

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    private int[]dp;
+    public int numDecodings(String s) {
+        if(s == null || s.length() == 0)return 0;
+        int n = s.length();
+        dp = new int[n];
+        Arrays.fill(dp,-1);
+        return dfs(s,0);
+    }
+
+    private int dfs(String s, int idx){
+        if(idx == s.length())return 1;
+        if(s.charAt(idx) == '0')return 0;
+        if(dp[idx] != -1)return dp[idx];
+        int result = dfs(s,idx+1);
+        if(idx < s.length()-1){
+            if((s.charAt(idx) == '1') || ( s.charAt(idx) == '2' && s.charAt(idx+1) < '7')){
+                result += dfs(s,idx+2);
+            }
+        }
+        return dp[idx] = result;
+    }
+}
