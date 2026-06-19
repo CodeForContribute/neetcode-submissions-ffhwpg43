9e5f877c46c84aa2bd0c8624eda3b280bc1ea35c
Raushan Singh <19raushaniiitu@gmail.com>
2026-06-19 13:40:33 +0530

    Add: jump-game - submission-5

---
+++ Data Structures & Algorithms/jump-game/submission-5.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] arr) {
+        int n = arr.length;
+        int goal = n-1;
+        for(int i = n-2; i >= 0; i--){
+            if(i + arr[i] >= goal){
+                goal = i;
+            }
+        }
+        return goal == 0;
+    }
+}
