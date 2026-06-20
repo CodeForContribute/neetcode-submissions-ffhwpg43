f06d5c755b7c66439325abd597b1d498d69d7dda
Raushan Singh <19raushaniiitu@gmail.com>
2026-06-20 13:26:11 +0530

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    private Integer[][] dp;
+
+    public int change(int amount, int[] arr) {
+        if (arr == null || arr.length == 0)
+            return 0;
+        int n = arr.length;
+        dp = new Integer[n + 1][amount + 1];
+        return dfs(arr, 0, amount);
+    }
+
+    private int dfs(int[] arr, int idx, int target) {
+        if (target < 0 || idx >= arr.length)
+            return 0;
+        if (target == 0)
+            return 1;
+        if (dp[idx][target] != null)
+            return dp[idx][target];
+        int take = dfs(arr, idx, target - arr[idx]);
+        int skip = dfs(arr, idx + 1, target);
+        return dp[idx][target] = take + skip;
+    }
+}
